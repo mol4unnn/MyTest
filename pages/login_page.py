fb186754d20484f6938d5ba72eb5e9bcedba555e
mol4unnn <ko_te@mail.ru>
2022-09-12 14:21:21 +0300

    Переход по Page Object в одном тесте

---
+++ pages/login_page.py
@@ -0,0 +1,20 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        assert "login" in self.browser.current_url, "Должна быть ссылка на странцу введения логина"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.FORM_REG), "Должна быть форма регистрации"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.FORM_LOGIN), "Должна быть форма логина-пароля"

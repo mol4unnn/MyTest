cecb62242a746fe4018f1a6405e38f5e48882479
mol4unnn <ko_te@mail.ru>
2022-09-14 21:36:29 +0300

    тест добавления товара в корзину

--- pages/locators.py
+++ pages/locators.py
@@ -2,7 +2,17 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    BUTTON_BUSKET = (By.CLASS_NAME, "btn-add-to-basket")
+    PRODUCT_ADD_BUSKET = (By.TAG_NAME, "h1")
+    PRODUCT_IN_BUSKET =(By.LINK_TEXT, "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/")
 
 class LoginPageLocators():
     FORM_REG = (By.CSS_SELECTOR, "#register_form")
     FORM_LOGIN = (By.CSS_SELECTOR, "#login_form")
+
+class ProductPageLocators():
+    BASKET_TOTAL = (By.CSS_SELECTOR, ".alertinner p strong")
+    BASKET_STRONG_NAMES = (By.CSS_SELECTOR, ".alertinner strong")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    BASKET_ADD_BTN = (By.CLASS_NAME, "btn-add-to-basket")
---
+++ test_product_page.py
@@ -0,0 +1,10 @@
+from .pages.product_page import ProductPage
+
+def test_guest_can_add_product_to_cart(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_add_to_basket_btn()
+    page.add_item_to_basket()
+    page.solve_quiz_and_get_code()
+    page.should_item_in_basket()
